7d88b2bac617648e8eefb5d287f906cf522a46ab
skdksldk <tnwns1485@naver.com>
2023-08-17 08:47:18 +0900

    Add CTA section

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,12 +2,14 @@ import Logo from "../assets/Logo.png";
 import HeroImage from "../assets/HeroImage.jpg";
 import Post1Image from "../assets/posts/post1.png";
 import PostProfileImage from "../assets/posts/post-profile.png";
+import CtaImage from "../assets/CtaImage.png";
 
 const images = {
   Logo,
   HeroImage,
   Post1Image,
   PostProfileImage,
+  CtaImage
 };
 
 export default images;
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import MainLayout from "../../components/MainLayout";
 import Articles from "./container/Articles";
 import Hero from "./container/Hero";
+import CTA from "./container/CTA";
 
 const HomePage = () => {
   return (
     <MainLayout>
       <Hero />
       <Articles />
+      <CTA />
     </MainLayout>
   );
 };
