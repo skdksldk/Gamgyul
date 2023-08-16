f848ec723432bba5e4eaf72e2bccf5a27a9a973c
skdksldk <tnwns1485@naver.com>
2023-08-17 08:26:04 +0900

    Add Article Card

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,13 @@
 import Logo from "../assets/Logo.png";
 import HeroImage from "../assets/HeroImage.jpg";
+import Post1Image from "../assets/posts/post1.png";
+import PostProfileImage from "../assets/posts/post-profile.png";
 
 const images = {
   Logo,
   HeroImage,
+  Post1Image,
+  PostProfileImage,
 };
 
 export default images;
