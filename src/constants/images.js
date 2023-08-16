24be017e7e2d00b7487e20d5f1e59652365ae6f9
skdksldk <tnwns1485@naver.com>
2023-08-17 08:57:08 +0900

    Add Footer

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,13 +3,15 @@ import HeroImage from "../assets/HeroImage.jpg";
 import Post1Image from "../assets/posts/post1.png";
 import PostProfileImage from "../assets/posts/post-profile.png";
 import CtaImage from "../assets/CtaImage.png";
+import Symbollogo from "../assets/Symbollogo.png";
 
 const images = {
   Logo,
   HeroImage,
   Post1Image,
   PostProfileImage,
-  CtaImage
+  CtaImage,
+  Symbollogo,
 };
 
 export default images;
