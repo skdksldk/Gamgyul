1d97f7149bfa48b0459483d57ec07312ae935da7
skdksldk <tnwns1485@naver.com>
2023-09-16 04:05:24 +0900

    Add Comment

---
+++ src/components/comments/CommentForm.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+const CommentForm = ({ 
+  btnLabel, 
+  formSubmitHanlder,
+  formCancelHandler = null,
+  initialText = "",
+}) => {
+  const [value, setValue] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    formSubmitHanlder(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="flex felx-col items-end border border-primary rounded-lg p-4">
+        <textarea 
+         className="w-full focus:outline=none"
+         rows="5"
+         placeholder="Leave your comment here..."
+         value={value}
+         onChange={(e)=> setValue(e.target.value)}
+        />
+        <div className="flex items-center gap-x-2 pt-2">
+          {formCancelHandler && (
+            <button
+              onClick={formCancelHandler}
+              className="px-6 py-2.5 rounded-lg border border-red-500 text-red-500"
+            >
+              Cancel
+            </button>
+          )}
+        <button
+         type="submit"
+         className="px-6 py-2.5 rounded-lg bg-primary tex-white font-semibold"
+        >
+          {btnLabel}
+        </button>
+        </div>
+      </div>
+    </form>
+  )
+}
+
+export default CommentForm
