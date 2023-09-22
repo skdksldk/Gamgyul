002fa0f09c70cc8e2fb33331db810692c2e2f474
skdksldk <tnwns1485@naver.com>
2023-09-22 13:33:07 +0900

    Add New Comment

--- src/components/comments/CommentForm.jsx
+++ src/components/comments/CommentForm.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const CommentForm = ({ 
-  btnLabel, 
+const CommentForm = ({
+  btnLabel,
   formSubmitHanlder,
   formCancelHandler = null,
   initialText = "",
+  loading = false,
 }) => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState(initialText);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -16,13 +17,13 @@ const CommentForm = ({
 
   return (
     <form onSubmit={submitHandler}>
-      <div className="flex felx-col items-end border border-primary rounded-lg p-4">
-        <textarea 
-         className="w-full focus:outline=none"
-         rows="5"
-         placeholder="Leave your comment here..."
-         value={value}
-         onChange={(e)=> setValue(e.target.value)}
+      <div className="flex flex-col items-end border border-primary rounded-lg p-4">
+        <textarea
+          className="w-full focus:outline-none bg-transparent"
+          rows="5"
+          placeholder="Leave your comment here..."
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
         />
         <div className="flex flex-col-reverse gap-y-2 items-center gap-x-2 pt-2 min-[420px]:flex-row">
           {formCancelHandler && (
@@ -33,16 +34,18 @@ const CommentForm = ({
               Cancel
             </button>
           )}
-        <button
-         type="submit"
-         className="px-6 py-2.5 rounded-lg bg-primary tex-white font-semibold"
-        >
-          {btnLabel}
-        </button>
+          <button
+            disabled={loading}
+            type="submit"
+            className="px-6 py-2.5 rounded-lg bg-primary
+            text-white font-semibold disabled:opacity-70 disabled:cursor-not-allowed"
+          >
+            {btnLabel}
+          </button>
         </div>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default CommentForm
+export default CommentForm;
